47234fc142cc4a16a7b19ab4c1264cffbcd85e1b
juratdev <juratdev@gmail.com>
2023-02-16 13:25:32 +0500

    School project

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const loaded = () => console.log("Sahifa yuklandi...");
+const sidebar = document.getElementById("sidebar");
+const cards = document.getElementById("cards");
+const selected = document.getElementById("selected");
+
+const toggleSidebar = () => {
+  if (sidebar.classList.contains("hide")) {
+    sidebar.classList.remove("hide");
+  } else sidebar.classList.add("hide")
+}
+
+const keyUp = (e) => {
+  if (e.key == "Escape") sidebar.classList.add("hide");
+}
+
+
+for (const card of cards.children) {
+  card.onclick = () => {
+    const menu = card.children[0].innerHTML;
+    selected.innerHTML = menu;
+  }
+}
