49a5fb4c35ea47cfb3439c02ba689e2555ca2966
juratdev <juratdev@gmail.com>
2023-02-18 10:20:33 +0500

    New dark mode function added JavaScript and styles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const loaded = () => console.log("Sahifa yuklandi...");
+
 const sidebar = document.getElementById("sidebar");
 const cards = document.getElementById("cards");
 const selected = document.getElementById("selected");
-const textTitle = document.querySelector(".title__text");
+const darkBtn = document.querySelector(".darkBtn");
 
 const toggleSidebar = () => {
   if (sidebar.classList.contains("hide")) {
@@ -14,11 +15,15 @@ const keyUp = (e) => {
   if (e.key == "Escape") sidebar.classList.add("hide");
 }
 
-textTitle.style.color = "#001845";
 for (const card of cards.children) {
   card.onclick = () => {
     const menu = card.children[0].innerHTML;
     selected.innerHTML = menu;
-    selected.style.color = "#001845";
   }
+}
+
+darkBtn.onclick = () => {
+  if (document.body.id === "dark") {
+    document.body.id = "";
+  } else document.body.id = "dark";
 }
